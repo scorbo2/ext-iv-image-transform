49fa1ab4d75e18dbd445c9f17a25f24e2391bb37
Steve Corbett <steve@corbett.ca>
2026-02-01 21:04:47 -0700

    Issue #4 - Upgrade to ImageViewer 3.0

--- src/main/java/ca/corbett/imageviewer/extensions/imagetransform/TransformImageAction.java
+++ src/main/java/ca/corbett/imageviewer/extensions/imagetransform/TransformImageAction.java
@@ -1,42 +1,49 @@
 package ca.corbett.imageviewer.extensions.imagetransform;
 
+import ca.corbett.extras.EnhancedAction;
 import ca.corbett.imageviewer.ui.ImageInstance;
 import ca.corbett.imageviewer.ui.MainWindow;
 
-import javax.swing.AbstractAction;
 import java.awt.event.ActionEvent;
 import java.io.File;
 
 /**
  * Launches the TransformImageDialog for the selected image, if there is one.
  *
- * @author scorbo2
+ * @author <a href="https://github.com/scorbo2">scorbo2</a>
  */
-public class TransformImageAction extends AbstractAction {
+public class TransformImageAction extends EnhancedAction {
 
-    public TransformImageAction() {
-        super("Transform this image...");
+    private static final String NAME = "Transform image...";
+    private static TransformImageAction instance;
+
+    private TransformImageAction() {
+        super(NAME);
+    }
+
+    public static TransformImageAction getInstance() {
+        if (instance == null) {
+            instance = new TransformImageAction();
+        }
+        return instance;
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         ImageInstance currentImage = MainWindow.getInstance().getSelectedImage();
         if (currentImage.isEmpty()) {
-            MainWindow.getInstance().showMessageDialog("Transform image", "Nothing selected.");
+            MainWindow.getInstance().showMessageDialog(NAME, "Nothing selected.");
             return;
         }
 
         // Ensure correct file format:
         File file = currentImage.getImageFile();
-        if (!file.getName().toLowerCase().endsWith("jpg")
-            && !file.getName().toLowerCase().endsWith("jpeg")
-            && !file.getName().toLowerCase().endsWith("png")) {
-            MainWindow.getInstance().showMessageDialog("Transform image",
+        if (!TransformImageExtension.fileExtensionIsSupported(file)) {
+            MainWindow.getInstance().showMessageDialog(NAME,
                                                        "Image transformation can currently only be performed on jpeg or png images.");
             return;
         }
 
         new TransformImageDialog(file).setVisible(true);
     }
-
 }
